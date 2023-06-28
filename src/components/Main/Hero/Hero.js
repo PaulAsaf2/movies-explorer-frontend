83c706b3d75261b80625e2e8911b24d2b019fa8a
Pavel Asafov <pavelasaf@yandex.ru>
2023-06-28 13:01:00 +0600

    настроил header c Pixel Perfect

--- src/components/Main/Hero/Hero.js
+++ src/components/Main/Hero/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero() {
   return (
-    <div className='hero_container'>
+    <div className='hero'>
       <h1 className='hero__title'>Учебный проект студента факультета Веб-разработки.</h1>
     </div>
   )
