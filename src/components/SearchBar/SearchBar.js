be58a0b63450cfa6c5acada95a032ede96eb9e4e
Pavel Asafov <pavelasaf@yandex.ru>
2023-07-08 21:41:35 +0600

    Настройка Movie для 320px

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Media from "react-media";
 import ToggleFilms from "./ToggleFilms/ToggleFilms";
 
 function handleToggle(state) {
@@ -7,18 +8,38 @@ function handleToggle(state) {
 
 function SearchBar() {
   return (
-    <section className="search-bar">
-      <form className="search-bar__form">
-        <input className="search-bar__input" type="search" placeholder="Фильм" />
-        <button className="search-bar__button" type="submit"></button>
-      </form>
-      <div className="search-bar__separation-line"></div>
-      <ToggleFilms
-        toggled={true}
-        onClick={handleToggle}
-      />
-      <p className="search-bar__text">Короткометражки</p>
-    </section>
+    <Media query={{ maxWidth: 320 }}>
+      {matches =>
+        matches ? (
+          <>
+            <section className="search-bar">
+              <form className="search-bar__form">
+                <input className="search-bar__input" type="search" placeholder="Фильм" />
+                <button className="search-bar__button" type="submit"></button>
+              </form>
+            </section >
+            <div className="search-bar__container">
+              <ToggleFilms toggled={true} onClick={handleToggle} />
+              <p className="search-bar__text">Короткометражки</p>
+            </div>
+          </>
+        ) : (
+          <section className="search-bar">
+            <form className="search-bar__form">
+              <input className="search-bar__input" type="search" placeholder="Фильм" />
+              <button className="search-bar__button" type="submit"></button>
+            </form>
+            <div className="search-bar__separation-line"></div>
+            <ToggleFilms
+              toggled={true}
+              onClick={handleToggle}
+            />
+            <p className="search-bar__text">Короткометражки</p>
+          </section >
+        )
+      }
+    </Media >
+
   )
 }
 
