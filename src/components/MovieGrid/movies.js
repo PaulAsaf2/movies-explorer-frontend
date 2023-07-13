ee13edc1d12702551506704bf00b289e5b421701
Pavel Asafov <pavelasaf@yandex.ru>
2023-07-13 10:42:07 +0600

    настроил Movies 320px c тегом main

--- src/components/MovieGrid/movies.js
+++ src/components/MovieGrid/movies.js
@@ -64,45 +64,6 @@ const filmsArray = [
     thumbnail: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
     movieId: "5"
   },
-  {
-    country: "Germany",
-    director: "August",
-    duration: 120,
-    year: 2023,
-    description: "Good Gans",
-    image: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    trailerLink: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    nameRU: "Просто фильм",
-    nameEN: "Good Gans",
-    thumbnail: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    movieId: "6"
-  },
-  {
-    country: "Germany",
-    director: "August",
-    duration: 120,
-    year: 2023,
-    description: "Good Gans",
-    image: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    trailerLink: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    nameRU: "Просто фильм",
-    nameEN: "Good Gans",
-    thumbnail: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    movieId: "7"
-  },
-  {
-    country: "Germany",
-    director: "August",
-    duration: 120,
-    year: 2023,
-    description: "Good Gans",
-    image: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    trailerLink: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    nameRU: "Просто фильм",
-    nameEN: "Good Gans",
-    thumbnail: "https://img.freepik.com/free-photo/old-cement-wall-texture_1149-1280.jpg?w=740&t=st=1688570408~exp=1688571008~hmac=1e5eee5d54080e0a75cca7b73647b80a5a3595b900785c18c9a3673d2e74ed17",
-    movieId: "8"
-  },
 ]
 
 export default filmsArray;
