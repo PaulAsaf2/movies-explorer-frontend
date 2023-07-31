f479247974db819159d7ba98caa32c3bc155223d
Pavel Asafov <pavelasaf@yandex.ru>
2023-07-31 19:15:34 +0600

    удалил credentials: include из authorize

--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -19,7 +19,6 @@ export function register(name, email, password) {
 export function authorize(email, password) {
   return fetch(`${baseUrl}/signin`, {
     method: 'POST',
-    credentials: 'include',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password })
   })
